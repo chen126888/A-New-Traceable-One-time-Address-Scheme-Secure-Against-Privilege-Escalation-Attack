0edb8c0f3e97d4a965c93dc3ac6e57a058ed159d
chen126888 <41716407+chen126888@users.noreply.github.com>
2025-06-25 15:50:19 +0800

    back to old version

--- schemes_demo/app/api/trace.py
+++
@@ -1,6 +0,0 @@
-from flask import request, jsonify
-from app.api import bp
-
-@bp.route('/trace/identity', methods=['POST'])
-def trace_identity():
-    return jsonify({"message": "Trace API working"})
